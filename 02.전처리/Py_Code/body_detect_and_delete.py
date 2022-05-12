d73aafe6d2d4ace72b20ce9336e1b7a3dae20200
K-osA <evbayooooo@gmail.com>
2022-05-12 18:06:14 +0900

    Updated body detect code

--- 02.전처리/Py_Code/body_detect_and_delete.py
+++ 02.전처리/Py_Code/body_detect_and_delete.py
@@ -5,8 +5,7 @@ import cv2
 
 dir_path = ['./크롤링/긴팔티셔츠', './크롤링/민소매티셔츠', './크롤링/반팔티셔츠', './크롤링/피케_카라티셔츠', './크롤링/후드티셔츠']
 
-full_body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "./haarcascade_fullbody.xml")
-
+lower_body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "./haarcascade_lowerbody.xml")
 
 for in_dir in dir_path:
     cnt = 0
@@ -15,11 +14,11 @@ for in_dir in dir_path:
             img = cv2.imread(in_dir + '/' + file)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            full_bodys_cascade = full_body_cascade.detectMultiScale(gray, 1.01, 10, 0, minSize=(100, 100))
+            lower_bodys_cascade = lower_body_cascade.detectMultiScale(gray, 1.01, 10, 0, minSize=(100, 100))
 
-            if len(full_bodys_cascade) > 0:
+            if len(lower_bodys_cascade) > 0:
                 os.remove(in_dir + '/' + file)
                 print("deleted in " + in_dir + '/' + file)
-                print("detected full_body count: " + str(len(full_bodys_cascade)))
+                print("detected body count: " + str(len(lower_bodys_cascade)))
                 cnt += 1
     print("deleted " + str(cnt) + " files in " + in_dir)
